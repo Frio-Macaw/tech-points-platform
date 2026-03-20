d7688f50b20466415cbb1be3a3b2d1422cd7b39b
Macaw <nattakarn@macawth.com>
2026-03-20 15:46:33 +0700

    Update liff-starter.js

--- liff-starter.js
+++ liff-starter.js
@@ -1,6 +1,6 @@
 // ===== CONFIG (ของคุณ) =====
 const LIFF_ID = "2009127023-sgWJernO";
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzyzubdy2RHBv88vZmylzxy2_tL1FzQypLUSmQCbeH9Dq3KOAIKPrkA23DvZln9zx783Q/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwGDVLrwM-AdqEMNICFj775XG2p769ipK6jABirndrPgQuJgazemdWCWemcBxfNf53l/exec";
 
 // ===== INIT =====
 async function liffInit() {
